9ce0afe72b175254a8c46c174775ed42287432c6
Jacob Bernstein <jacobbernstein@Jacobs-MacBook-Pro.local>
2017-10-15 13:04:59 +0900

    Adding React Router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,31 @@
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Switch = ReactRouter.Switch;
 
 class App extends React.Component {
   render() {
     return (
-     <div className = 'container'> 
-     		<Popular />
-     </div>
+    	<Router>
+	     <div className = 'container'> 
+	     		<Nav />
+	     		<Switch>
+		     		<Route exact path = '/' component = {Home} />
+		     		<Route path='/popular' component={Popular} />
+		     		<Route exact path='/battle' component={Battle} />
+
+		     		//will only be shown if no others paths active
+		     		<Route render = {function(){
+		     			return (<p>Not found</p>)
+		     		}} />
+	     		</Switch>
+	     </div>
+	    </Router>
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,17 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' }, // when this runs, looks for babel in package.json
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
